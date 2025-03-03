2d0f06dbf8b88c1bae61f46f7b685796cc0efc8a
IsaacVerm <isaacverm@gmail.com>
2025-03-03 17:04:42 +0100

    manually remove fields curl-overview-search-min.sh no impact after testing

--- curl-overview-search-min.sh
+++ curl-overview-search-min.sh
@@ -4,35 +4,19 @@ curl 'https://www.vlaanderen.be/api/overview-search' \
   -H 'Accept: application/json' \
   -H 'authorization: Basic d2VicGxhdGZvcm1kZXY6d2VicGxhdGZvcm1kZXY=' \
   -H 'content-type: application/json' \
-  -H 'Origin: https://www.vlaanderen.be' \
-  -H 'Referer: https://www.vlaanderen.be/werken-voor-vlaanderen/vacatures' \
   --data '{
     "page": {
       "offset": 0,
-      "limit": 12
+      "limit": 1
     },
     "filter": {
       "contentType": {
         "IN": ["Job"]
       },
-      "visibility": {
-        "hub": "cc0f4502-9afd-42cf-b71f-31e43937d855"
-      },
       "collectionFilters": {
         "contentSubtypeData__sources": {
           "IN": ["VO"]
-        },
-        "contentSubtypeData__internal": {
-          "EQUAL": false
         }
       }
-    },
-    "orderBy": {
-      "publicationDate": "DESC"
-    },
-    "resolverContext": {
-      "language": "nl",
-      "revision": "default"
-    },
-    "includeFacets": true
+    }
   }'
